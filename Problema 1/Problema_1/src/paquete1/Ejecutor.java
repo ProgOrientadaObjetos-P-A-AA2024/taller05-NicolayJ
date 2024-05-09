773990af5de29f78e51de510abf635519a0f3559
NicolayJ <dnjimenez6@utpl.edu.ec>
2024-05-09 00:25:55 -0500

    Listo

---
+++ Problema 1/Problema_1/src/paquete1/Ejecutor.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete1;
+
+import paquete2.LibretaNotas;
+import java.util.Scanner;
+
+public class Ejecutor {
+
+    public static void main(String[] args) {
+        Scanner tecla = new Scanner(System.in);
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("--------o MENU:");
+            System.out.println("1. Crear objeto Estudiante (2 calificaciones)");
+            System.out.println("2. Crear objeto Estudiante (3 calificaciones)");
+            System.out.println("3. Salir");
+            System.out.println("---------------------------------------------");
+            System.out.print("Ingrese una opcion: ");
+            int opcion = tecla.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("=========================================");
+                System.out.print("  Ingrese nombre del estudiante: ");
+                String nombre = tecla.next();
+                System.out.print("  Ingrese calificacion 1: ");
+                double calificacion1 = tecla.nextDouble();
+                System.out.print("  Ingrese calificacion 2: ");
+                double calificacion2 = tecla.nextDouble();
+
+                LibretaNotas notas = new LibretaNotas(nombre, calificacion1, 
+                        calificacion2);
+                notas.calcularPromedioNotas();
+
+                System.out.printf("%s",notas);
+            } else if (opcion == 2) {
+                System.out.println("=========================================");
+                System.out.print("Ingrese nombre del estudiante: ");
+                String nombre = tecla.next();
+                System.out.print("  Ingrese calificacion 1: ");
+                double calificacion1 = tecla.nextDouble();
+                System.out.print("  Ingrese calificacion 2: ");
+                double calificacion2 = tecla.nextDouble();
+                System.out.print("  Ingrese calificacion 3: ");
+                double calificacion3 = tecla.nextDouble();
+
+                LibretaNotas notas = new LibretaNotas(nombre, calificacion1,
+                        calificacion2, calificacion3);
+                notas.calcularPromedioNotas();
+
+                System.out.printf("%s",notas);
+            } else if (opcion == 3) {
+                System.out.println("Saliendo...");
+                bandera = false;
+            } else {
+                System.out.println("Ingrese una opcion existente");
+            }
+        }
+    }
+}
---
+++ Problema 1/Problema_1/src/paquete2/LibretaNotas.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete2;
+
+public class LibretaNotas {
+
+    private String nombreEstudiante;
+    private double calificacion01;
+    private double calificacion02;
+    private double calificacion03;
+    private double promedioNota;
+    
+    public LibretaNotas(String n, double c1, double c2, double c3) {
+        nombreEstudiante = n;
+        calificacion01 = c1;
+        calificacion02 = c2;
+        calificacion03 = c3;
+    }
+
+    public LibretaNotas(String n, double c1, double c2) {
+        nombreEstudiante = n;
+        calificacion01 = c1;
+        calificacion02 = c2;
+        calificacion03 = 10;
+    }
+    
+    public void establecerNombreEstudiante(String x) {
+        nombreEstudiante = x;
+    }
+
+    public void establecerCalificacion01(double x) {
+        calificacion01 = x;
+    }
+
+    public void establecerCalificacion02(double x) {
+        calificacion02 = x;
+    }
+
+    public void establecerCalificacion03(double x) {
+        calificacion03 = x;
+    }
+
+    public void calcularPromedioNotas() {
+        promedioNota = (calificacion01 + calificacion02 + calificacion03) / 3;
+    }
+
+    public String obtenerNombreEstudiante() {
+        return nombreEstudiante;
+    }
+
+    public double obtenerCalificacion01() {
+        return calificacion01;
+    }
+
+    public double obtenerCalificacion02() {
+        return calificacion02;
+    }
+
+    public double obtenerCalificacion03() {
+        return calificacion03;
+    }
+
+    public double obtenerPromedioNotas() {
+        return promedioNota;
+    }
+    @Override
+    public String toString(){
+        String cadena = String.format("\n\nLIBRETAS DE CALIFICACIONES \n"
+                + " Nombre del Estudiante: %s\n"
+                + " Calificacion 1: %.2f\n"
+                + " Calificacion 2: %.2f\n"
+                + " Calificacion 3: %.2f\n"
+                + " Promedio Final: %.3f\n",
+                nombreEstudiante,
+                obtenerCalificacion01(),
+                obtenerCalificacion02(),
+                obtenerCalificacion03(),
+                obtenerPromedioNotas());
+        return cadena;
+    }
+}
